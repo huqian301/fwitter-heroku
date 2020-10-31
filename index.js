4241d7affbd071bcf1380d7bb3e76ece7c9ce043
huqian301 <huqian301@gmail.com>
2020-10-31 11:50:44 -0700

    add

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const routes = require('./routes');
 require('./services/passport');
 
 const app = express();
+const PORT = process.env.PORT || 3001;
 
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static('client/build'));
@@ -15,6 +16,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(routes);
 
-app.listen(3001, () => {
+
+app.listen(PORT, () => {
   console.log('Server started listening on PORT http://localhost:3001');
 });
